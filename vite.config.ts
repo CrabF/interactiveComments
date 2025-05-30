2f6a7684d1105c3c4e9b3d5c929442285df3b240
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2025-05-30 09:52:36 +0300

    fix: html, replace path to vite-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      assets: path.resolve(__dirname, "./src/assets"),
-      components: path.resolve(__dirname, "./src/components"),
-    },
-  },
+  plugins: [react(), svgr(), tsconfigPaths()],
 });
